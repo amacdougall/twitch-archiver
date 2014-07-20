90a899a6e426b5a826a1534d461715016b98c418
Alan MacDougall <smoke@alanmacdougall.com>
2014-07-20 14:40:31 -0400

    Zero-pad output files for better sorting; show current/total in CLI.
    Output files are now named "part_n" if there are fewer than 10 files in
    all; "part_nn" if there are fewer than 100; and so on.
    
    CLI output now includes the total number of files, as in "downloading
    part 1/12."

--- archive.rb
+++ archive.rb
@@ -28,14 +28,16 @@ unless Dir["#{output_dir}/*"].empty?
   FileUtils.rm_rf(Dir.glob("dir/to/remove/*")) # remove directory contents
 end
 
+total_files = file_json.length
+
 file_json.each_with_index do |file_data, index|
   tempfile = Tempfile.new(["archive_download", ".flv"])
 
   begin
-    # download and write to tempfile
     progress_bar = nil
 
-    puts "Downloading #{file_data["title"]} part #{index}..."
+    # download and write to tempfile
+    puts "Downloading #{file_data["title"]} part #{index}/#{total_files}..."
     tempfile.write(open(file_data["transcode_file_urls"]["transcode_480p"], {
       :content_length_proc => lambda do |t|
         if t && 0 < t
@@ -48,12 +50,18 @@ file_json.each_with_index do |file_data, index|
       end
     }).read)
 
-    puts "Transcoding #{file_data["title"]} part #{index}..."
     # transcode to MP4
+    puts "Transcoding #{file_data["title"]} part #{index}/#{total_files}..."
     movie = FFMPEG::Movie.new(tempfile.path)
     progress_bar = ProgressBar.new("...", 1.0)
-    movie.transcode(File.join(output_dir, "part_#{index}.mp4")) do |progress|
-      progress_bar.set([progress, 1.0].min) # clamp to max of 1.0
+
+    # pad the index in the filename to the max digits; i.e. if 14 files, name
+    # them "00", "01", ...
+    padded_index = index.to_s.rjust(total_files.to_s.length, "0")
+
+    movie.transcode(File.join(output_dir, "part_#{padded_index}.mp4")) do |progress|
+      # clamp to max of 1.0; streamio-ffmpeg sometimes issues 1.000001 progress.
+      progress_bar.set([progress, 1.0].min)
     end
   ensure
     tempfile.close
